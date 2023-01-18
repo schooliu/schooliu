5441a0cdb28e2892072d4e1baa54d3d79f410066
yami2200 <TheYami22@gmail.com>
2023-01-18 15:39:26 +0100

    Add configuration for Auth0 authentication provider.

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,10 +1,12 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
+import Auth0Provider from "next-auth/providers/auth0";
 // Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
 import { env } from "../../../env/server.mjs";
 import { prisma } from "../../../server/db";
+
+
 
 export const authOptions: NextAuthOptions = {
   // Include user.id on session
@@ -19,9 +21,13 @@ export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   adapter: PrismaAdapter(prisma),
   providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
+    Auth0Provider({
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      clientId: env.AUTH0_CLIENT_ID,
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      clientSecret: env.AUTH0_CLIENT_SECRET,
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      issuer: env.AUTH0_ISSUER
     }),
     /**
      * ...add more providers here
