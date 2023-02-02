ac2ae4752e45ddbd2fb694941bb4d0ac45dbe7d7
yami2200 <TheYami22@gmail.com>
2023-02-02 11:50:29 +0100

    doc: add comments and error message for university router.

--- src/server/api/routers/university.ts
+++ src/server/api/routers/university.ts
@@ -3,8 +3,15 @@ import { z } from "zod";
 import { createTRPCRouter, publicProcedure, protectedProcedure } from "../trpc";
 
 export const universityRouter = createTRPCRouter({
+
+    /**
+     * Get a university by its ID.
+     */
     getById: publicProcedure
-        .input(z.object({ id: z.string() }))
+        .input(z.object({ id: z.string({
+                required_error: "ID is required",
+                invalid_type_error: "ID must be a string",
+            }) }))
         .query(({ input, ctx }) => {
             return ctx.prisma.university.findUnique({
                 where: {
@@ -13,12 +20,22 @@ export const universityRouter = createTRPCRouter({
             });
         }),
 
+    /**
+     * Get all universities
+     */
     getAll: publicProcedure.query(({ ctx }) => {
         return ctx.prisma.university.findMany();
     }),
 
+    /**
+     * Get number of user in a university.
+     * @param id The ID of the university to get the number of users.
+     */
     getUserCount: publicProcedure
-        .input(z.object({ id: z.string() }))
+        .input(z.object({ id: z.string({
+                required_error: "ID is required",
+                invalid_type_error: "ID must be a string",
+            }) }))
         .query(({ input, ctx }) => {
             return ctx.prisma.user.count({
                 where: {
@@ -29,8 +46,15 @@ export const universityRouter = createTRPCRouter({
             });
         }),
 
+    /**
+     * Get number of courses in a university.
+     * @param id The ID of the university to get the number of courses.
+     */
     getCoursesCount: publicProcedure
-        .input(z.object({ id: z.string() }))
+        .input(z.object({ id: z.string({
+                required_error: "ID is required",
+                invalid_type_error: "ID must be a string",
+            }) }))
         .query(({ input, ctx }) => {
             return ctx.prisma.course.count({
                 where: {
@@ -45,8 +69,15 @@ export const universityRouter = createTRPCRouter({
             });
         }),
 
+    /**
+     * Get number of chapters in a university.
+     * @param id The ID of the university to get the number of chapters.
+     */
     getChaptersCount: publicProcedure
-        .input(z.object({ id: z.string() }))
+        .input(z.object({ id: z.string({
+                required_error: "ID is required",
+                invalid_type_error: "ID must be a string",
+            }) }))
         .query(({ input, ctx }) => {
             return ctx.prisma.chapter.count({
                 where: {
