171ae6b98b1448058359ffae7f86d53a1aa9e669
Mael Kerichard <38262536+Pixselve@users.noreply.github.com>
2023-02-09 20:53:10 +0100

    chore: remove head

--- app/[university]/(navigation)/[sectionSlug]/[sectionYearSlug]/[subjectSlug]/head.tsx
+++
@@ -1,7 +0,0 @@
-export default async function Head({ params }: { params: { university: string, sectionSlug: string, sectionYearSlug: string, subjectSlug: string } }) {
-  return (
-    <>
-      <title>📚 {params.subjectSlug} - Cursus</title>
-    </>
-  );
-}
